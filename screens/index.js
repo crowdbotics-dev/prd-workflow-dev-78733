74436ac88d6223c45bb1155c8b6e9b558e7d57dc
Crowdbotics <engineering@crowdbotics.com>
2023-04-24 18:14:12 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,129 +1,24 @@
-import React, { useState, useEffect } from "react";
-import {
-  Text,
-  StyleSheet,
-  View,
-  Image,
-  ScrollView,
-  TextInput
-} from "react-native";
+import React from "react";
+import { Text, StyleSheet, View, Image, Pressable } from "react-native";
 
-const DirectMessagesScreen = (params) => {
-  const [message, setMessage] = useState("");
-  const [user1, setUser1] = useState({});
-  const [user2, setUser2] = useState({});
-  const [conversation, setConversation] = useState([]);
-  useEffect(() => {
-    setUser1({
-      name: "User1",
-      image: require("./assets/profile1.png"),
-      isOnline: true
-    });
-    setUser2({
-      name: "Username",
-      image: require("./assets/profile2.png"),
-      isOnline: true
-    });
-  }, []);
-  useEffect(() => {
-    setConversation([
-      {
-        id: 1,
-        sender: user1,
-        text: "lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-        sentTime: "12:00 PM"
-      },
-      {
-        id: 2,
-        sender: user2,
-        text: "lorem ipsum dolor sit amet",
-        sentTime: "01:15 PM"
-      },
-      {
-        id: 3,
-        sender: user2,
-        text: "lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-        sentTime: "01:15 PM"
-      },
-      {
-        id: 4,
-        sender: user1,
-        text: "lorem ipsum dolor sit amet",
-        sentTime: "01:15 PM"
-      },
-      {
-        id: 5,
-        sender: user2,
-        text: "lorem ipsum dolor sit amet",
-        sentTime: "01:15 PM"
-      },
-      {
-        id: 6,
-        sender: user1,
-        text: "lorem ipsum dolor sit amet",
-        sentTime: "01:15 PM"
-      }
-    ]);
-  }, [user1, user2]);
+const UploadMediaScreen = (params) => {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.headerImage}>
-          <Image source={user2.image} style={styles.headerImage} />
-          {(user2.isOnline && (
-            <Image
-              source={require("./assets/onlineIcon.png")}
-              style={styles.activityIconHeader}
-            />
-          )) ||
-            null}
+      <View style={styles.menu}>
+        <View style={styles.menuItem}>
+          <Text style={styles.menuItemText}>Take photo or video</Text>
+          <Image source={require("./assets/cameraIcon.png")} />
         </View>
-        <View style={styles.profileName}>
-          <Text style={styles.profileNameText}>{user2.name}</Text>
+        <View style={styles.menuItem}>
+          <Text style={styles.menuItemText}>Photo Library</Text>
+          <Image source={require("./assets/libraryIcon.png")} />
         </View>
-        <View style={styles.icons}>
-          <Image source={require("./assets/phoneIcon.png")} />
-          <Image source={require("./assets/videoIcon.png")} />
+        <View style={styles.menuItem}>
+          <Text style={styles.menuItemText}>Browse</Text>
+          <Image source={require("./assets/menuIcon.png")} />
         </View>
       </View>
-      <ScrollView style={styles.body}>
-        {conversation.map((message, index) => (
-          <ConversationElement message={message} key={index} />
-        ))}
-      </ScrollView>
-      <View style={styles.footer}>
-        <View style={styles.camera}>
-          <Image
-            source={require("./assets/cameraIcon.png")}
-            style={styles.cameraIcon}
-          />
-        </View>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Type a message"
-            onChangeText={(text) => setMessage(text)}
-            value={message}
-            autoCorrect={false}
-            autoCapitalize="none"
-            autoFocus={false}
-          />
-          <Image
-            source={require("./assets/emojiIcon.png")}
-            style={styles.smileyIcon}
-          />
-          <Image
-            source={require("./assets/voiceIcon.png")}
-            style={styles.voiceIcon}
-          />
-        </View>
-        <View style={styles.send}>
-          <Image
-            source={require("./assets/sendIcon.png")}
-            style={styles.sendIcon}
-          />
-        </View>
-      </View>
+      <Button buttonText="Cancel" outline={true} />
     </View>
   );
 };
@@ -131,156 +26,74 @@ const DirectMessagesScreen = (params) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: "#fff",
+    padding: 20,
+    justifyContent: "flex-end"
   },
-  header: {
+  menuItem: {
     flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
     justifyContent: "space-between",
-    alignItems: "center",
-    padding: 10,
-    marginHorizontal: 20,
-    backgroundColor: "#F1F1F1",
-    borderRadius: 10,
-    height: 50
+    borderBottomWidth: 1,
+    borderColor: "#ccc"
   },
-  headerImage: {
-    flex: 1,
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    resizeMode: "contain",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  profileName: {
-    flex: 2
-  },
-  profileNameText: {
-    fontSize: 16
-  },
-  icons: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around"
-  },
-  activityIconHeader: {
-    width: 10,
-    height: 10,
-    resizeMode: "contain",
-    position: "absolute",
-    right: 20,
-    top: 25
-  },
-  body: {
-    flex: 1,
-    marginTop: 10
-  },
-  footer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 10,
-    margin: 10
-  },
-  inputContainer: {
-    width: "80%"
-  },
-  input: {
-    paddingLeft: 15,
-    borderRadius: 10,
-    backgroundColor: "#F1F1F1"
-  },
-  smileyIcon: {
-    position: "absolute",
-    right: 40,
-    top: 10,
-    opacity: 0.5
-  },
-  voiceIcon: {
-    top: 14,
-    right: 15,
-    position: "absolute",
-    opacity: 0.5
+  menuItemText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#000",
+    textAlign: "left"
   }
 });
-export default DirectMessagesScreen;
+export default UploadMediaScreen;
 
-const ConversationElement = ({ message }) => {
-  const boxAlignment = {
-    flexDirection: message.sender.name === "User1" ? "row" : "row-reverse"
+const Button = (params) => {
+  const btnStyle = {
+    backgroundColor: params.outline ? "#fff" : "#000",
+    borderColor: params.outline ? "#000" : "#fff",
+    borderWidth: 1
   };
-  const messageTextContainer = {
-    marginLeft: message.sender.name === "User1" ? 20 : 0,
-    marginRight: message.sender.name === "User1" ? 0 : 20,
-    backgroundColor: message.sender.name === "User1" ? "#FCF1D6" : "#F9D8D9"
+  const btnText = {
+    color: params.outline ? "#000" : "#fff"
   };
   return (
-    <View style={[ConversationElementStyles.messageContainer, boxAlignment]}>
-      <View style={ConversationElementStyles.profileImageContainer}>
-        <Image
-          source={message.sender.image}
-          style={ConversationElementStyles.profileImage}
-        />
-        {(message.sender.isOnline && (
-          <Image
-            source={require("./assets/onlineIcon.png")}
-            style={ConversationElementStyles.activityIcon}
-          />
-        )) ||
-          null}
-      </View>
-      <View
-        style={[
-          ConversationElementStyles.messageTextContainer,
-          messageTextContainer
-        ]}
-      >
-        <Text style={ConversationElementStyles.messageText}>
-          {message.text}
-        </Text>
-        <Text style={ConversationElementStyles.messageTime}>
-          {message.sentTime}
-        </Text>
-      </View>
+    <View style={buttonStyles.btnContainer}>
+      <Pressable style={[buttonStyles.btn, btnStyle]} onPress={params.onPress}>
+        <Text style={[buttonStyles.btnText, btnText]}>{params.buttonText}</Text>
+        <View style={styles.childrenContainer}>{params.children}</View>
+      </Pressable>
     </View>
   );
 };
 
-const ConversationElementStyles = StyleSheet.create({
-  messageContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    margin: 10,
-    justifyContent: "flex-start",
-    marginVertical: 20
-  },
-  messageTextContainer: {
-    minHeight: 70,
-    paddingVertical: 10,
+const buttonStyles = StyleSheet.create({
+  btnContainer: {
+    paddingTop: 10,
     paddingHorizontal: 20,
+    justifyContent: "center",
+    marginTop: 20
+  },
+  btn: {
+    backgroundColor: "black",
+    height: 50,
+    width: "100%",
+    padding: 10,
+    paddingHorizontal: 25,
     borderRadius: 10,
     justifyContent: "center",
-    alignItems: "flex-start",
-    width: "70%"
+    alignItems: "center",
+    shadowColor: "rgba(0, 0, 0, 0.2)",
+    elevation: 10,
+    flexDirection: "row"
   },
-  messageText: {
-    lineHeight: 20,
-    fontSize: 14,
-    color: "#000",
-    fontWeight: "bold",
-    textAlign: "left"
+  btnText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold"
   },
-  activityIcon: {
-    position: "absolute",
-    right: 0,
-    top: 40
-  },
-  messageTime: {
-    position: "absolute",
-    right: 5,
-    bottom: -20,
-    color: "grey",
-    fontSize: 12
+  childrenContainer: {
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
