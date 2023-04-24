82fe029dd0d2e571f18374c4c2e68657cbdb86a6
Crowdbotics <engineering@crowdbotics.com>
2023-04-24 18:14:03 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,167 +1,125 @@
-import React, { useState, useEffect } from "react";
-import {
-  Text,
-  StyleSheet,
-  View,
-  SafeAreaView,
-  FlatList,
-  Image
-} from "react-native";
+import React from "react";
+import { Text, View, StyleSheet, TouchableHighlight, TextInput } from "react-native";
 
-const ArchiveContent = (params) => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    setData([
-      {
-        id: 1,
-        archiveDate: "7 MAR",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 2,
-        archiveDate: "10 APR",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 3,
-        archiveDate: "13 MAY",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 4,
-        archiveDate: "16 JUN",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 5,
-        archiveDate: "5 JAN",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 6,
-        archiveDate: "18 JUL",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 7,
-        archiveDate: "21 AUG",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 8,
-        archiveDate: "24 SEP",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 9,
-        archiveDate: "27 OCT",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 10,
-        archiveDate: "30 NOV",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 11,
-        archiveDate: "1 DEC",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 12,
-        archiveDate: "4 JAN",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 13,
-        archiveDate: "30 NOV",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 14,
-        archiveDate: "1 DEC",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 15,
-        archiveDate: "4 JAN",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 16,
-        archiveDate: "30 NOV",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 17,
-        archiveDate: "1 DEC",
-        imageSrc: require("./assets/crowdbotics.png")
-      },
-      {
-        id: 18,
-        archiveDate: "4 JAN",
-        imageSrc: require("./assets/crowdbotics.png")
-      }
-    ]);
-  }, []);
+const SupportSendFeedbackScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        style={styles.list}
-        data={data}
-        numColumns={3}
-        renderItem={({ item }) => <Post post={item} />}
-        keyExtractor={(item) => item.id.toString()}
-        columnWrapperStyle={{
-          justifyContent: "space-between"
-        }}
-      />
-    </SafeAreaView>
-  );
-};
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.labelText}>
+          Email address
+        </Text>
+        <Input
+          placeholder='Enter'
+        />
+      </View>
+      <View style={styles.Textarea}>
+        <Text style={styles.labelText}>
+          Message
+        </Text>
+        <TextInput style={styles.TextAreaInput}
+          numberOfLines={5}
+          multiline={true}
+          placeholder='Enter'
+          placeholderTextColor='#ddd'
+        />
+      </View>
 
-const Post = ({ post }) => {
-  return (
-    <View style={styles.postContainer}>
-      <Image source={post.imageSrc} style={styles.image} />
-      <Text style={styles.archiveDate}>{post.archiveDate}</Text>
+      <View style={styles.SubmitBtn}>
+        <Button>Submit</Button>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    padding: 20,
     flex: 1,
-    backgroundColor: "#fff"
+    height: "100%",
+    backgroundColor: "white"
   },
-  list: {
-    marginTop: 10,
-    flex: 1,
-    height: "100%"
+  labelText: {
+    fontSize: 14,
+    fontWeight: "400",
+    marginBottom: 10,
+    marginLeft: 15
   },
-  postContainer: {
-    height: 125,
-    width: 125,
+  Textarea: {
+    marginTop: 30
+  },
+  TextAreaInput: {
+    height: 160,
+    borderWidth: 1,
+    borderColor: "#C4C4C4",
     borderRadius: 10,
-    margin: 3
+    textAlignVertical: "top",
+    padding: 10
   },
-  image: {
-    height: 125,
-    width: 125,
-    borderRadius: 10
-  },
-  archiveDate: {
-    position: "absolute",
-    top: 5,
-    right: 5,
-    backgroundColor: "#fff",
-    paddingHorizontal: 10,
-    paddingVertical: 3,
-    borderRadius: 6,
-    fontSize: 10,
-    fontWeight: "700",
-    textAlign: "center",
-    color: "#000"
+  SubmitBtn: {
+    marginTop: 100
   }
 });
-export default ArchiveContent;
+
+export default SupportSendFeedbackScreen;
+
+const Button = (props) => {
+  return (
+    <TouchableHighlight onPress={props.onPress} underlayColor='#DDDDDD'>
+      <View style={[btnStyles.button, {
+        backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
+        height: props.height ? props.height : 49,
+        borderWidth: props.borderWidth ? props.borderWidth : 0,
+        borderColor: props.borderColor ? props.borderColor : "#000000"
+      }]}>
+        <Text style={[btnStyles.text, { color: props.color ? props.color : "#ffffff" }]}>{props.children}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const btnStyles = StyleSheet.create({
+  button: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 15
+  }
+});
+
+const Input = (props) => {
+  return (
+    <View>
+      <TextInput
+        style={textStyles.input}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChangeText={(num) => props.setValue(num)}
+        placeholderTextColor='#ddd'
+        editable={props.editable !== false}
+      />
+      {props.errorText ? <Text style={textStyles.error}>{props.errorText}</Text> : null}
+    </View>
+  );
+};
+
+const textStyles = StyleSheet.create({
+  input: {
+    backgroundColor: "#fff",
+    height: 53,
+    borderColor: "#C4C4C4",
+    color: "#000",
+    borderRadius: 10,
+    fontSize: 14,
+    borderWidth: 1,
+    paddingHorizontal: 10
+  },
+  error: {
+    fontSize: 13,
+    color: "#FA060D",
+    paddingTop: 8
+  }
+});
